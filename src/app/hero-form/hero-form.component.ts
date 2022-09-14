5c34103f4ee941dd1cc37d007bbd064db0e86552
stagiaire118 <stagiaire118@rtl.be>
2022-09-14 11:36:03 +0200

    premier push

---
+++ src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.css']
+})
+export class HeroFormComponent implements OnInit {
+
+  submitted = false;
+
+  powers = [
+    'Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'
+  ];
+  model = new Hero(18,'Dr IQ', this.powers[0], 'Chuck Overstreet');
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(){
+    this.submitted = true;
+  }
+
+  newHero(){
+    this.model = new Hero(42,'','');
+  }
+}
